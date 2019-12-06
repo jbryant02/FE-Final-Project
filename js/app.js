7dfa0dfee4080aa9e189fab104a9ed26c181d9c6
jbryant02 <bryant.justin92@gmail.com>
2019-12-05 23:03:26 -0500

    Fixes js issue regarding selection of map

--- js/app.js
+++ js/app.js
@@ -1,33 +1,44 @@
 //Select Region
-var region = [".South", ".Northeast", ".Great-Lakes", ".Southwest", ".Utah", ".Midwest", ".Rockies", ".Northwest", ".California", ".Alaska", ".Hawaii"]
-
+var region = ["South", "Northeast", "Great-Lakes", "Southwest", "Utah", "Midwest", "Rockies", "Northwest", "California", "Alaska", "Hawaii"];
+var $regions;
 
 $('.Map').children().on('click', function (event) {
     console.log(event.target);
-    
-    var $regions = $(event.target).attr("class");
+    $regions = $(event.target).attr("class");
     console.log($regions);
-    $regions.hide(100);
-    
+    for (var i = 0; i < region.length; i++) {
+        if ($regions !== region[i]) {
+            $("." + region[i]).animate({
+                opacity: 0.35
+            }, 1500)
+        }
+    }
 });
 
-        /* for(var i=0; i< buttons.length; i++){
-            buttons[i].hidden = true;
-        }
+$('.reset').on('click', function (event) {
+ for(var i = 0; i < region.length; i++){
+     $("." + region[i]).removeAttr('style');
+ }
+});
+   
 
-        $(class).on('click', function (event) {
-            
-            console.log(event.target);
-            $(this).hide(1000);
-        });
 
-        //$('.State').click(function() {
-        //    var myClass = $(this).attr("class");
-        //   alert(myClass);
-        //});
-        //if( $(event.target).hasClass(".State") ){
-        //    $.each(region, function (index, value))
-        //    
-        //           }
-        //    console.log(event.target);
-        //    $(this).hide(1000); */
+
+// for(var i=0; i< buttons.length; i++){
+//    buttons[i].hidden = true;
+//}
+
+//$(class).on('click', function (event) {
+
+//  console.log(event.target);
+//    $(this).hide(1000);
+//    });
+
+//$('.State').click(function() {
+//    var myClass = $(this).attr("class");
+//   alert(myClass);
+//});
+//if( $(event.target).hasClass(".State") ){
+//    $.each(region, function (index, value))
+//           }
+//    console.log(event.target);
