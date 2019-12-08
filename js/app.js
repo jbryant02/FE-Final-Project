d6ed9ed84b239934c9dc1b11b7c0c2a4e6c2c355
jbryant02 <bryant.justin92@gmail.com>
2019-12-08 02:37:57 -0500

    added a lot of photos and changed many items into bootstrap.

--- js/app.js
+++ js/app.js
@@ -2,25 +2,28 @@
 var region = ["South", "Northeast", "Great-Lakes", "Southwest", "Utah", "Midwest", "Rockies", "Northwest", "California", "Alaska", "Hawaii"];
 var $regions;
 
+
 $('.Map').children().on('click', function (event) {
-    console.log(event.target);
-    $regions = $(event.target).attr("class");
-    console.log($regions);
-    for (var i = 0; i < region.length; i++) {
-        if ($regions !== region[i]) {
-            $("." + region[i]).animate({
-                opacity: 0.35
-            }, 1500)
+    if ($(this).attr("id") !== "reset") { //excludes reset button and overall map from click to prevent opacity changes.
+        console.log(event.target);
+        $regions = $(event.target).attr("class");
+        console.log($regions);
+        for (var i = 0; i < region.length; i++) { //loops through region to animate
+            if ($regions !== region[i]) {
+                $("." + region[i]).animate({
+                    opacity: 0.15
+                }, 1500)
+            }
         }
     }
 });
 
 $('.reset').on('click', function (event) {
- for(var i = 0; i < region.length; i++){
-     $("." + region[i]).removeAttr('style');
- }
+    for (var i = 0; i < region.length; i++) { //loops through regions to restyle
+        $("." + region[i]).removeAttr('style');
+    }
 });
-   
+
 
 
 
@@ -42,3 +45,15 @@ $('.reset').on('click', function (event) {
 //    $.each(region, function (index, value))
 //           }
 //    console.log(event.target);
+
+/* $('.Map').children().on('click', function (event) {
+    if ($(event.target).attr("id") !== "reset"){
+    var checkState = function stateCheckFunction() {
+        for (var loop = 0; loop < region.length; loop++) {
+        if (this === region[loop]){
+        console.log(checkState);
+        return region[loop];
+            }
+        }
+    }
+    }}); */
